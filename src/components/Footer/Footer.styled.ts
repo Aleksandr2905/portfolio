1c813ba4bef667edef625228a88c52e3d76ca654
Aleksandr2905 <natalukha@ukr.net>
2024-06-07 23:37:14 +0300

    added Footer

--- src/components/Footer/Footer.styled.ts
+++ src/components/Footer/Footer.styled.ts
@@ -1,1 +1,54 @@
-// import styled from "styled-components";
+import styled from "styled-components";
+
+export const Wrapper = styled.footer`
+  padding-top: 40px;
+  background-color: var(--bg-footer);
+`;
+
+export const ContentBlock = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  max-width: 1108px;
+  margin: 0 auto;
+  padding-bottom: 40px;
+  border-bottom: 1px solid var(--main-typography);
+`;
+
+export const ContactsBlock = styled.div`
+  display: flex;
+  gap: 40px;
+`;
+
+export const Contact = styled.a`
+  display: flex;
+  gap: 16px;
+  font-family: Montserrat, sans-serif;
+  font-size: 18px;
+  font-weight: 400;
+  line-height: normal;
+  color: var(--main-typography);
+  transition: all 0.3s;
+
+  & svg {
+    fill: var(--main-typography);
+  }
+
+  &:hover {
+    color: var(--light-blue);
+
+    & svg {
+      fill: var(--light-blue);
+    }
+  }
+`;
+
+export const Copyright = styled.p`
+  text-align: center;
+  padding: 20px 0;
+  font-family: Montserrat, sans-serif;
+  font-size: 16px;
+  font-weight: 400;
+  line-height: normal;
+  color: var(--main-typography);
+`;
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,32 @@
+import sprite from "../../assets/icons/sprite.svg";
+import Social from "../Social/Social";
+import * as s from "./Footer.styled";
+
 const Footer: React.FC = () => {
-  return <footer>Footer</footer>;
+  return (
+    <s.Wrapper>
+      <s.ContentBlock>
+        <s.ContactsBlock>
+          <s.Contact href="tel:+380660578505">
+            <svg width={24} height={24}>
+              <use href={`${sprite}#call`} />
+            </svg>
+            +380 660 578 505
+          </s.Contact>
+          <s.Contact href="mailto:natalukha.a.a@gmail.com">
+            <svg width={24} height={24}>
+              <use href={`${sprite}#email`} />
+            </svg>
+            natalukha.a.a@gmail.com
+          </s.Contact>
+        </s.ContactsBlock>
+        <Social />
+      </s.ContentBlock>
+      <s.Copyright>
+        © 2024 Oleksandr Natalukha. All rights reserved.
+      </s.Copyright>
+    </s.Wrapper>
+  );
 };
 
 export default Footer;
