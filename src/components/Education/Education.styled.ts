58495115eac6d112f9db40ab18d953f322a60f29
Aleksandr2905 <natalukha@ukr.net>
2024-06-08 01:05:04 +0300

    fix

--- src/components/Education/Education.styled.ts
+++ src/components/Education/Education.styled.ts
@@ -3,16 +3,17 @@ import styled from "styled-components";
 export const Wrapper = styled.div`
   display: flex;
   justify-content: center;
+  align-items: center;
   gap: 24px;
 `;
 
-export const Image = styled.img`
-  width: 180px;
-  height: 60px;
-  padding: 8px;
-  border-radius: 8px;
-  background-color: #fff;
-`;
+// export const Image = styled.img`
+//   width: 180px;
+//   height: 60px;
+//   padding: 8px;
+//   border-radius: 8px;
+//   background-color: #fff;
+// `;
 
 export const School = styled.h3`
   font-family: Montserrat, sans-serif;
--- src/components/Education/Education.tsx
+++ src/components/Education/Education.tsx
@@ -1,25 +1,24 @@
 import { Container } from "../../style/GlobalStyles";
 import Title from "../Title/Title";
 import * as s from "./Education.styled";
-import goit1x from "../../assets/image/goit@1x.png";
-import goit2x from "../../assets/image/goit@2x.png";
+// import goit1x from "../../assets/image/goit@1x.png";
+// import goit2x from "../../assets/image/goit@2x.png";
 
 const Education: React.FC = () => {
   return (
     <Container id="education">
       <Title title="Education" />
       <s.Wrapper>
-        <s.Image
+        {/* <s.Image
           srcSet={`${goit1x} 1x, ${goit2x} 2x`}
           src={goit1x}
           alt="logo goit"
           width={180}
           height={60}
-        />
-        <div>
-          <s.School>IT School GoIT</s.School>
-          <s.Specialist>Fullstack Developer | 2023 - 2024</s.Specialist>
-        </div>
+        /> */}
+
+        <s.School>IT School GoIT</s.School>
+        <s.Specialist>Fullstack Developer | 2023 - 2024</s.Specialist>
       </s.Wrapper>
     </Container>
   );
