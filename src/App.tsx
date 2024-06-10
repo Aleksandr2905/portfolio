d819bdc64ff8ff7e4c14cfb02a2c59be1c30e0fb
Aleksandr2905 <natalukha@ukr.net>
2024-06-10 23:39:04 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import TechSkills from "./components/TechSkills/TechSkills";
 import Education from "./components/Education/Education";
 import Projects from "./components/Projects/Projects";
 import Contact from "./components/Contact/Contact";
-import Footer from "./components/Footer/Footer";
+// import Footer from "./components/Footer/Footer";
 import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
 
 function App() {
