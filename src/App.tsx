4630c5c2f1743e1a48a6fd86ca5c16c4e54a41ad
Aleksandr2905 <natalukha@ukr.net>
2024-06-11 22:14:21 +0300

    fix scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import About from "./components/About/About";
@@ -9,6 +11,38 @@ import Footer from "./components/Footer/Footer";
 import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
 
 function App() {
+  const { hash } = useLocation();
+
+  useEffect(() => {
+    if (hash) {
+      const element = document.querySelector(hash);
+      if (element) {
+        element.scrollIntoView({ behavior: "smooth" });
+      }
+    }
+  }, [hash]);
+
+  const handleScroll = () => {
+    const scrollPosition = window.scrollY + window.innerHeight / 2;
+    const sections = document.querySelectorAll("section");
+    sections.forEach((section) => {
+      const { offsetTop, offsetHeight } = section;
+      if (
+        scrollPosition >= offsetTop &&
+        scrollPosition < offsetTop + offsetHeight
+      ) {
+        const id = `${section.id}`;
+        window.history.replaceState(null, "", id);
+        return;
+      }
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <>
       <Header />
