bc05c3d4586d76b8ce0d328477736c93aa333eed
Aleksandr2905 <natalukha@ukr.net>
2024-07-18 16:46:30 +0300

    Update CardProject.tsx

--- src/components/CardProject/CardProject.tsx
+++ src/components/CardProject/CardProject.tsx
@@ -13,8 +13,8 @@ const CardProject: React.FC<ICardProjectProps> = ({ project }) => {
         <s.Image
           src={image}
           alt="Photo project"
-          width='100%'
-          height='100%'
+          width={239}
+          height={132}
           loading="lazy"
         />
         <s.Description>
